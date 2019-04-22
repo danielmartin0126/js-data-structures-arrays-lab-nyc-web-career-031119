8d1bc05f985851a486697b4a54151d6954a97e12
Danny Martin <danielmartin0126@gmail.com>
2019-04-22 17:16:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ function removeLastDriver(){
 }
 
 function removeFirstDriver(){
-  let newDrivers = drivers.slice(0,drivers.length -1)
+  let newDrivers = drivers.slice(1,drivers.length -1)
   return newDrivers
 }
